2dcb8c6f13d1cb0bd513eb6f6e2fca901a85fea4
MossyRain <msk419@gmail.com>
2026-08-29 09:13:06 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,16 +1,1 @@
-const CACHE = 'holo-lab-v0.1';
-const ASSETS = ['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./icon-180.png','./icon-512.png'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
+const C='mira-v02',A=['./','./index.html','./style.css','./app.js','./mira-jii.jpg','./manifest.webmanifest','./icon-180.png','./icon-512.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
